5b166c5ddd6600f3e786ebff93fb5cc0bb486dd8
Dawa Lama <https://github.com/dawa10000>
2026-07-06 11:23:42 +0545

    add

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,13 @@ const nextConfig = {
   reactCompiler: true,
   instrumentationHook: true,
   serverExternalPackages: ["@xenova/transformers"],
+  outputFileTracingIncludes: {
+    "/api/ai-search/**": [
+      "./node_modules/@xenova/transformers/**",
+      "./node_modules/onnxruntime-web/**",
+      "./node_modules/onnxruntime-node/**",
+    ],
+  },
 };
 
 export default nextConfig;
